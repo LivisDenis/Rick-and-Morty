01452c0466ef6dbcd2a2b35bf17a4ef329128cb5
Denis Chumachenko <deniskachumachenko1@gmail.com>
2023-03-06 04:24:30 +0200

    Fix bugs

--- app/characters/[id]/page.tsx
+++ app/characters/[id]/page.tsx
@@ -4,17 +4,18 @@ export async function generateStaticParams() {
   const charactersCount = (await caller.getCharactersInfo()).response.count;
 
   return Array.from({ length: charactersCount }, (_, i) => i + 1).map((id) => ({
-    id: id
+    id: id.toString()
   }));
 }
 
 interface CharacterProps {
   params: {
-    id: number;
+    id: string;
   };
 }
+
 const Character = async ({ params }: CharacterProps) => {
-  const { response } = await caller.getCharacter({ params: { id: params.id } });
+  const { response } = await caller.getCharacter({ params: { id: +params.id } });
 
   return (
     <div>
