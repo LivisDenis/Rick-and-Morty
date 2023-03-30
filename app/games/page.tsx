8d42aaf141acb0f6bbb8aab15b8fda3e2dd773c6
Denis Chumachenko <deniskachumachenko1@gmail.com>
2023-03-30 03:10:49 +0300

    Styled the main page with games and added paths

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,8 +1,31 @@
+import Link from 'next/link';
+
+import { ROUTES } from '@/src/utils';
+
 export const metadata = {
   title: '🔫 Games',
   description: 'Omg morty ?'
 };
 
-const Games = () => <section>Games</section>;
+const gamesList = [
+  {
+    titleGame: 'Dead or Alive or Unknown',
+    tagline: 'Who or What is died or alive or unknown',
+    link: ROUTES.DEAD_ALIVE_UNKNOWN
+  }
+];
+
+const Games = () => (
+  <div className='grid grid-cols-2 max-[740px]:grid-cols-1 gap-x-6 gap-y-4'>
+    {gamesList.map((game, i) => (
+      <Link key={i} href={`${ROUTES.GAMES}/${game.link}`}>
+        <div className='w-full transition ease-out duration-150 flex flex-col max-w-[500px] hover:scale-105 rounded-[10px] bg-gray-600 p-8'>
+          <div className='text-[24px] font-bold text-slate-200'>{game.titleGame}</div>
+          <span className='mt-3 text-slate-400 text-[18px]'>{game.tagline}</span>
+        </div>
+      </Link>
+    ))}
+  </div>
+);
 
 export default Games;
--- src/utils/constants/routes.ts
+++ src/utils/constants/routes.ts
@@ -1,6 +1,7 @@
 export const ROUTES = {
   ROOT: '/',
   GAMES: '/games',
+  DEAD_ALIVE_UNKNOWN: 'dead-alive-unknown',
   CHARACTERS: '/characters',
   EPISODES: '/episodes/0',
   LOCATIONS: '/locations'
